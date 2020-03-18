c655d2ca7d5e343c752900242322decb781db28c
Constantine Mars <constantine.mars@gmail.com>
2020-03-17 22:31:04 -0700

    Getting photo description json from api

--- app/src/main/java/com/example/hsexercise/feature/FeatureActivity.kt
+++ app/src/main/java/com/example/hsexercise/feature/FeatureActivity.kt
@@ -3,14 +3,33 @@ package com.example.hsexercise.feature
 import android.os.Bundle
 import com.example.hsexercise.R
 import com.example.hsexercise.common.BaseActivity
+import com.example.hsexercise.feature.network.PhotoNetworkProvider
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 
 class FeatureActivity : BaseActivity<FeatureViewModel>() {
     override val viewModelClass = FeatureViewModel::class.java
     override val layoutResId = R.layout.activity_feature
+    val compositeDisposable = CompositeDisposable()
 
     override fun provideViewModelFactory() = FeatureViewModel.Factory()
 
     override fun onViewLoad(savedInstanceState: Bundle?) {
         // todo: write code here
+        compositeDisposable.add(PhotoNetworkProvider.providePhotoService().listRepos()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe (
+                { photos -> Timber.d("photos size = %d", photos.size) },
+                {error -> Timber.e(error)}
+            )
+        )
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.dispose()
+        super.onDestroy()
     }
 }
