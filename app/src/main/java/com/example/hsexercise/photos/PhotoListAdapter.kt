1407f10bb90f9ad3d3c5bd5d7997c92941e11e16
Constantine Mars <constantine.mars@gmail.com>
2020-03-19 01:23:29 -0700

    Image loading progress bar, crop style

--- app/src/main/java/com/example/hsexercise/photos/PhotoListAdapter.kt
+++ app/src/main/java/com/example/hsexercise/photos/PhotoListAdapter.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestListener
 import com.example.hsexercise.R
 import com.example.hsexercise.photos.model.Photo
 import kotlinx.android.synthetic.main.photo_item.view.*
@@ -33,8 +35,16 @@ class PhotoListAdapter internal constructor(
         val current = photos[position]
         holder.authorTextView.text = current.author
         holder.dimensionsTextView.text = String.format("%d x %d", current.width, current.height)
+
+        val circularProgressDrawable = CircularProgressDrawable(context)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.start()
+
         Glide.with(context)
             .load(current.download_url)
+            .placeholder(circularProgressDrawable)
+            .centerCrop()
             .into(holder.imageView);
     }
 
