76b3b7fc65b189389b8746005472bf2dccaae916
Constantine Mars <constantine.mars@gmail.com>
2020-03-18 20:14:39 -0700

    App component added to app class

--- app/src/main/java/com/example/hsexercise/App.kt
+++ app/src/main/java/com/example/hsexercise/App.kt
@@ -1,8 +1,14 @@
 package com.example.hsexercise
 
 import android.app.Application
+import com.example.hsexercise.common.di.AppComponent
 import com.example.hsexercise.common.di.DaggerAppComponent
+import com.example.hsexercise.common.di.modules.ApplicationModule
 
 class App: Application() {
-    val appComponent = DaggerAppComponent.create()
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.builder()
+            .applicationModule(ApplicationModule(applicationContext))
+            .build()
+    }
 }
